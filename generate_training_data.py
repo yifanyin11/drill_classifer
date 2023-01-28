597dabe7fe6c1d3466d2fa494dad4f6c8ff61cd1
yifanyin11 <96314762+yifanyin11@users.noreply.github.com>
2023-01-28 16:15:27 -0500

    Delete generate_training_data.py

--- generate_training_data.py
+++
@@ -1,8 +0,0 @@
-from frame_reader import FrameReader
-from annotator import Annotator
-
-if __name__ == "__main__":
-    fr = FrameReader("C:\\Users\\yifan\\Desktop\\MLAssessmentData")
-    fr.read_all_frames()
-    annotator = Annotator('C:\\Users\\yifan\\work\\drill_bit_classifier\\data')
-    annotator.generate_annotation()
